e2ae36af4b7990b9a7e6f90ba0c6b8b2b2f93c22
Nasif-21 <nasifshitab01@gmail.com>
2024-02-13 12:13:42 +0600

    ProjectPage done

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,32 +1,69 @@
 import City from "../assest/advcity.jpg"
+import Crime  from "../assest/crimep.jpg"
+import hpp   from "../assest/hny.jpg"
+import aline  from "../assest/airline.jpg"
+import  nws from "../assest/news.jpg"
+import lib  from "../assest/library-shelf.jpg"
+import scrRec  from "../assest/scrrec.png"
+import  Book from "../assest/notebook.jpg"
+
+
 
 const ProjectCardData=[
     {
         imgsrc:City,
         title:"Advance Java City",
-        text: "This project is based on a city portal using basic java coding",
+        text: "Designed an intuitive user interface with responsive design and implemented eﬃcient database queries for data retrieval. Demonstrated strong programming skills, problem- solving abilities, and attention to detail in delivering a functional and user-friendly City Portal.",
         view:"https://github.com/Nasif-21/Adance-Java-City",
 
     },
     {
-        imgsrc:City,
-        title:"Advance Java City",
-        text: "This project is based on a city portal using basic java coding",
-        view:"https://github.com/Nasif-21/Adance-Java-City",
+        imgsrc:Crime,
+        title:"Crime detection",
+        text: "Led development of Crime Detection and Police Portal in C# and SQL Server. Implemented real-time crime reporting with geolocation. Designed secure portal for case management and evidence storage. Delivered robust, scalable solution enhancing public safety through technology.",
+        view:"https://github.com/Nasif-21/Crime-Detection",
 
     },
     {
-        imgsrc:City,
-        title:"Advance Java City",
-        text: "This project is based on a city portal using basic java coding",
-        view:"https://github.com/Nasif-21/Adance-Java-City",
+        imgsrc:hpp,
+        title:"Happy New Year (Computer Graphics project)",
+        text: "Created interactive New Year graphics project with OpenGL/SFML. Implemented day-night toggle feature with smooth transitions. Designed animations including fireworks, confetti, and countdown timers.",
+        view:"https://github.com/Nasif-21/Happy-New-Year",
 
     },
     {
-        imgsrc:City,
-        title:"Advance Java City",
-        text: "This project is based on a city portal using basic java coding",
-        view:"https://github.com/Nasif-21/Adance-Java-City",
+        imgsrc:aline,
+        title:"Airline management system",
+        text: "Developed Airline Management System using HTML for structure, CSS for styling, PHP for server-side logic, and JavaScript for interactivity. Designed flight scheduling, ticket reservations, passenger management, and inventory control functionalities. Proficient in full-stack web development, database management, and UI design. Delivered reliable, user-friendly system with strong problem-solving skills, meeting aviation industry requirements.",
+        view:"https://github.com/Nasif-21/Web_Tech_Final_Project",
+
+    },
+    {
+        imgsrc:nws,
+        title:"News App",
+        text: " Developed a dynamic and responsive news application, NewsHub, utilizing React.js and integrating the powerful NewsAPI for real-time news updates. This project aimed to provide users with an intuitive and engaging platform to stay informed on current events across various categories.",
+        view:"https://github.com/Nasif-21/News_App",
+
+    },
+    {
+        imgsrc:lib,
+        title:"Library Management System",
+        text: "Conducted end-to-end testing of a Library Management System using Selenium WebDriver. Developed and executed test cases, documented test plans, cases, and reports. Collaborated with the development team to identify and resolve defects, ensuring a high-quality, bug-free system.",
+        view:"https://github.com/Nasif-21/SQT",
+
+    },
+    {
+        imgsrc:scrRec,
+        title:"Clippy Screen recorder",
+        text: "The Clippy Screen Recorder App Design project is a meticulously crafted user interface concept created using Figma. This design aims to provide a seamless and intuitive experience for users interested in screen recording, featuring interactive elements, dark and light modes, a secure login/signup page, and a collaborative community editing and showcase platform.",
+        view:"https://www.figma.com/community/file/1301400893342645602/clippy-app",
+
+    },
+    {
+        imgsrc:Book,
+        title:"iNotebook",
+        text: "Developed the iNotebook application, featuring a secure user authentication system and an intuitive user interface built with React.js for an enhanced experience. Designed and implemented a custom Express.js API for efficient backend operations, seamlessly integrating MongoDB for scalable data storage. Enabled users to perform essential note operations such as addition, updating, and deletion, enhancing productivity. Demonstrated full-stack proficiency by combining frontend aesthetics with a secure and efficient backend, showcasing a robust and feature-rich solution for efficient note-taking and organization.",
+        view:"https://github.com/Nasif-21/inotebook",
 
     },
 
